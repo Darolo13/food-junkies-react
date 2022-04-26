530192523754f00b8088e1643ae4222bfbceadde
Darolo13 <davidromero1244@gmail.com>
2022-04-25 21:10:49 -0700

    Add favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Initial Scaffolding</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
